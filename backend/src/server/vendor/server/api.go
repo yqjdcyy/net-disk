33d976e8ec44d4877288e89c553785dfa101fc49
yqjdcyy <yqjdcyy@gmail.com>
2019-10-08 23:40:50 +0800

    接口开发

--- backend/src/server/vendor/server/api.go
+++ backend/src/server/vendor/server/api.go
@@ -7,7 +7,8 @@ import (
 	"path/filepath"
 	"encoding/json"
 	"strconv"
-
+	"os"
+	"io/ioutil"
 	"config"
 	// "bitbucket.org/ansenwork/ilog"
 )
@@ -19,12 +20,83 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 		case http.MethodGet: 
 			pageFiles(w, r)
 		case http.MethodDelete:
-			fmt.Fprintf(w, "todo")
+			deleteFiles(w,r)
 	default:
 		fmt.Fprintf(w, "Method["+ r.Method+ "] is not support");
 	}
 
 }
+
+// deleteFiles 批量删除
+func deleteFiles(w http.ResponseWriter, r *http.Request){
+	
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "without list",http.StatusInternalServerError)
+        return
+	}
+	
+	var list []string
+    err = json.Unmarshal(body, &list)
+    if err != nil {
+		http.Error(w, "wrong format for file list",http.StatusInternalServerError)
+        return
+	}
+
+	// response
+	bytes, err := json.Marshal(removeFiles(list))
+	if err != nil {
+	  http.Error(w, err.Error(), http.StatusInternalServerError)
+	  return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytes)
+}
+
+
+func removeFiles(list []string) []string{
+
+	 fail := []string{}
+
+	for _, v := range list {
+		if !removeFile(v){
+			fail= append(fail, v)
+		}
+	}
+
+	return fail
+}
+
+func removeFile(path string) bool{
+
+	bFile:=isFile(path)
+	if !bFile{
+		return false
+	}
+
+	err:= os.Remove(path)
+	if nil!= err{
+		fmt.Println("fail to remove file["+ path+"]: "+ err.Error())
+		return false
+	}
+	
+	return true
+}
+
+func isFile(path string) bool{
+
+	if 0== len(path){
+		return false
+	}
+
+	info,err := os.Stat(path)
+	if nil!= err {
+		return false
+	}
+
+	return !info.IsDir()
+}
+
 
 // pageFiles 分页查询
 func pageFiles(w http.ResponseWriter, r *http.Request){
